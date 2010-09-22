abae148723c2c24e25e26a11f4a0ca71a00c797f
w12495 <w12495>
2010-09-22 04:33:49 +0000

    CRF-52: NEW: selenium tests - update to spoof user agents: All senlenium tests now spoof the user agent and run successfully. web.xml now contains userAgentFilter. Now depends on <dependency org="com.sensis" name="sdpCommon-web" rev="1.6-020" conf="default"/> and <dependency org="com.sensis" name="sdpCommon-testing" rev="0.4-008" conf="test->default"/>.

--- test/selenium/java/au/com/sensis/mobile/crf/showcase/selenium/BdpPageIntegrationTestCase.java
+++ test/selenium/java/au/com/sensis/mobile/crf/showcase/selenium/BdpPageIntegrationTestCase.java
@@ -4,13 +4,16 @@ import au.com.sensis.mobile.crf.showcase.selenium.fixture.HD800BdpPage;
 import au.com.sensis.mobile.crf.showcase.selenium.fixture.IphoneOS2xBdpPage;
 import au.com.sensis.mobile.crf.showcase.selenium.fixture.IphoneOS3xBdpPage;
 import au.com.sensis.mobile.crf.showcase.selenium.fixture.Nokia7600BdpPage;
+import au.com.sensis.wireless.test.selenium.UserAgent;
 
 /**
- * Tests the home page.
+ * Tests the BDP page.
  *
- * TODO
- *
- * In order to run this test start tomcat, then start the selenium server, run this as a JUnit test.
+ * In order to run this test, do either:
+ * <ol>
+ * <li>start tomcat, then run ant execute-system-tests</li>
+ * <li>run ant run-system-tests</li>
+ * </ol>
  *
  * @author Adrian.Koh2@sensis.com.au (based on Heather's work in Whereis Mobile)
  */
@@ -20,12 +23,14 @@ public class BdpPageIntegrationTestCase extends AbstractSeleniumIntegrationTestC
         openUrl("http://localhost:8080/bdp.action");
     }
 
+    protected final void openBdpWithUserAgent(final String userAgent) {
+        openUrlWithUserAgent("http://localhost:8080/bdp.action", userAgent);
+    }
+
     /**
      * Test the page for a specific device.
      */
     public void testHD800Device() throws Exception {
-        // TODO: setup user agent for HD800 device.
-
         openBdp();
 
         getPageFixtureFactory().createPageFixture(HD800BdpPage.class);
@@ -35,9 +40,7 @@ public class BdpPageIntegrationTestCase extends AbstractSeleniumIntegrationTestC
      * Test the page for a specific device.
      */
     public void testNokia7600Device() throws Exception {
-        // TODO: setup user agent for Nokia 7600 device.
-
-        openBdp();
+        openBdpWithUserAgent(UserAgent.NOKIA_7600.getUserAgentString());
 
         getPageFixtureFactory().createPageFixture(Nokia7600BdpPage.class);
     }
@@ -46,20 +49,17 @@ public class BdpPageIntegrationTestCase extends AbstractSeleniumIntegrationTestC
      * Test the page for a specific device.
      */
     public void testIphoneOS3xDevice() throws Exception {
-        // TODO: setup user agent for iphone OS 3.x device.
-
-        openBdp();
+        openBdpWithUserAgent(UserAgent.IPHONE_OS3_1.getUserAgentString());
 
         getPageFixtureFactory().createPageFixture(IphoneOS3xBdpPage.class);
+
     }
 
     /**
      * Test the page for a specific device.
      */
     public void testIphoneOS2xDevice() throws Exception {
-        // TODO: setup user agent for iphone OS 2.x device.
-
-        openBdp();
+        openBdpWithUserAgent(UserAgent.IPHONE_OS2_1.getUserAgentString());
 
         getPageFixtureFactory().createPageFixture(IphoneOS2xBdpPage.class);
     }
