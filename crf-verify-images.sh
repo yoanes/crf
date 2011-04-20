0aaa24385e6941ae0e225853fd460f4a09edb3d6
w12495 <w12495>
2011-04-20 04:12:59 +0000

    CRF-113 Build time verification of pre-generated scaled images: Added crf-verify-images.sh and set build.properties so that the build scripts will invoke this script when the verify-image-generation target is invoked.

---
+++ crf-verify-images.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# CRF wrapper for verify-gimages.sh so that we can specify the non-standard uiresources dir.
+
+./src/scripts/image-generation/verify-gimages.sh -r showcase/web/uiresources
